60bbe473e5e0a3164803786ccd5cb33da445b08e
ronxldz <erik.p.1208@icloud.com>
2023-07-05 23:15:08 -0400

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 // Write your Color component here
+const Color = ({color}) => {
+  return <div className={color}></div>
+}
 
 const App = () => {
   return (
@@ -7,7 +10,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+      <Color color="blue"/>
+      <Color color="green"/>
+      <Color color="red"/>
+      </div>
     </div>
   );
 };
